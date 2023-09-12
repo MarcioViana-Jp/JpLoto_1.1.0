c3ddc7a51f66ec1424d1c7440e08bcee02be5d5e
Marcio Viana <marciojjviana@gmail.com>
2023-09-13 00:34:53 +0900

    Implementando Cliente Webassembly

---
+++ JpLoto.Domain/Dto/Request/ChangePasswordRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JPLoto.Domain.Dto.Request;
+
+public class ChangePasswordRequest
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(50, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string SenhaAtual { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(50, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string NovaSenha { get; set; } = string.Empty;
+
+    [Compare(nameof(NovaSenha), ErrorMessage = "As senhas devem ser iguais")]
+    public string SenhaConfirmacao { get; set; } = string.Empty;
+}
---
+++ JpLoto.Site/Services/Account/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using JPLoto.Domain.Dto.Request;
+using JPLoto.Domain.Dto.Response;
+
+namespace JpLoto.Site.Services.Account
+{
+    public interface IAccountService
+    {
+        Task<LoginResponse> Login(LoginRequest request);
+        Task<RegisterResponse> Register(RegisterRequest request);
+        Task<RegisterResponse> ChangePassword(ChangePasswordRequest request);
+        //Task<bool> IsUserAuthenticated();
+        bool IsUserAuthenticated();
+    }
+}
